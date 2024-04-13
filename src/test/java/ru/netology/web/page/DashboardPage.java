b10946615f9ce94d5537f1f322f3a75a31fe7db4
Larisa test <alexandrova.uu@yandex.ru>
2024-04-13 19:39:38 +0300

    change BDD

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -25,10 +25,10 @@ public class DashboardPage {
         return extractBalance(text);
     }
 
-    public int getCardBalance(int index) {
-        var text = cards.get(index).getText();
-        return extractBalance(text);
-    }
+   // public int getCardBalance(int index) {
+  //      var text = cards.get(index).getText();
+  //      return extractBalance(text);
+  //  }
 
     public TransferPage selectCardToTransfer(DataHelper.CardInfo cardInfo) {
         cards.findBy(Condition.attribute("data-test-id", cardInfo.getTestId())).$("button").click();
